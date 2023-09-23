7a428c8075e8caeddb1972d66bee3eeb2bf79f9e
business tech hindi <133948484+businesstechhindi@users.noreply.github.com>
2023-09-23 19:20:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
-// Enable CORS for a specific origin
+// Enable CORS for a specific origin and allow credentials
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://book-store-frontend-6zte.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 // app.use(cors());
